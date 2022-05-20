884dd54d776016e6b824c5317d7b28ed1611a0c4
Henrique Ferreira <henriquebf@gmail.com>
2022-05-20 11:05:41 +0200

    add style components

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default MyApp;
