143bd77477a96f59bd365740ab3bc4219f5e932d
Henrique Ferreira <henriquebf@gmail.com>
2022-05-24 10:35:13 +0200

    fix: placehoder font size on mobile

--- web/components/sections/Placeholder.tsx
+++ web/components/sections/Placeholder.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import sizes from '@/config/sizes.json';
 import zIndexes from '@/config/zIndexes.json';
 import Container from '@/components/shared/Container';
 
@@ -22,7 +23,13 @@ const Placeholder: NextPage = () => {
         .logo {
           font-family: 'Lobster', cursive;
           text-align: center;
-          font-size: 10em;
+          font-size: 5em;
+        }
+
+        @media (min-width: ${sizes.container}) {
+          .logo {
+            font-size: 10em;
+          }
         }
       `}</style>
     </section>
